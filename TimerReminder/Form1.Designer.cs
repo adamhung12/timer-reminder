9d344f28926c9a64fac3da65e854be42ae8def18
Xeth Hung <adamhung12@gmail.com>
2020-09-03 23:02:18 +0800

    first version

---
+++ TimerReminder/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TimerReminder
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += this.textBox1_KeyDown;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sec";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 42);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.TopMost = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Location = new System.Drawing.Point(
+                System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width - this.Width,
+                System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - this.Height
+                );
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TimerReminder/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace TimerReminder
+{
+	public partial class Form1 : Form
+	{
+		public Form1(String s)
+		{
+			InitializeComponent();
+            if (s != null && s != "")
+            {
+                this.textBox1.Text = s;
+                triggerAction();
+            }
+        }
+
+        int h, m, s = 0;
+
+        bool updated = false;
+
+        void triggerAction()
+        {
+            String t = textBox1.Text;
+            updateTitle(t);
+            updateHour(t);
+            updateSec(t);
+            updateMin(t);
+
+            sec = s + m * 60 + h * 60 * 60;
+            i = 0;
+            textBox1.Visible = false;
+            label1.Visible = true;
+
+            updateLabelValue(sec, 0);
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000; // In milliseconds
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Elapsed += TimerElapsed;
+            timer.Start();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Enter)
+            {
+                triggerAction();
+            }
+        }
+
+        System.Timers.Timer timer = new System.Timers.Timer();
+        int i = 0;
+        int sec;
+        void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            
+            label1.Invoke((MethodInvoker)delegate
+            {
+
+                updateLabelValue(sec, ++i);
+                if (sec == i)
+                {
+                    this.Focus();
+                    this.BringToFront();
+                    this.Activate();
+                    label1.Text = "Done: " + h + " hours " + m + " mins " + s + " seconds";
+
+                    this.Location = new System.Drawing.Point(
+                        System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width/2 - this.Width/2,
+                        System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height/2 - this.Height/2
+                        );
+                    timer.Stop();
+                    timer.Dispose();
+                }
+            });
+            
+        }
+
+
+        void updateLabelValue(int sec, int i)
+        {
+            int n = sec - i++;
+            int h = n / 3600;
+            n = n % 3600;
+            int m = n / 60;
+            n = n % 60;
+            int s = n;
+            label1.Text = h + " hours " + m + " mins " + s + " seconds";
+        }
+
+        private void updateHour(String s)
+        {
+            Regex hourReg = new Regex(@"(\d+)[h|H]");
+            Match mm = hourReg.Match(s);
+            if (mm.Success)
+            {
+                string s2 = mm.Groups[1].Value.Trim();
+                h = Int16.Parse(s2);
+            }
+        }
+        private void updateMin(String s)
+        {
+            Regex hourReg = new Regex(@"(\d+)[m|M]");
+            Match mm = hourReg.Match(s);
+            if (mm.Success)
+            {
+                string s2 = mm.Groups[1].Value.Trim();
+                m = Int16.Parse(s2);
+            }
+
+        }
+        private void updateSec(String s)
+        {
+            Regex secReg = new Regex(@"(\d+)[s|S]");
+            Match mm = secReg.Match(s);
+            if (mm.Success)
+            {
+                string s2 = mm.Groups[1].Value.Trim();
+                this.s = Int16.Parse(s2);
+            }
+
+        }
+
+
+        private void updateTitle(String s)
+        {
+            Regex titleReg = new Regex(@"\*\*(.*)");
+            Match m = titleReg.Match(s);
+            if (m.Success)
+            {
+                string ss = m.Value;
+                string s2 = m.Groups[1].Value.Trim();
+
+                Text = s2;
+                
+                Console.Out.WriteLine("");
+            }
+
+
+        }
+    }
+}
